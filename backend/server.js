703302b06189605ccf6683510f6fde6046a1b8c0
Pratha Agarwal <prathaagarwal1501@gmail.com>
2025-08-01 21:35:56 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://prathaagarwal.github.io/'],
+    origin: ['http://localhost:5173', 'https://prathaagarwal.github.io'],
     methods: ['GET', 'POST'],
     credentials: true
   }
